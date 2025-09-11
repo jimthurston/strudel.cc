87f8fda7eb68036c386ec6c311a4b9998004b948
Jim Thurston <jimthurston@gmail.com>
2025-09-11 12:48:55 +0100

    Update six-marimbas.js

--- six-marimbas.js
+++ six-marimbas.js
@@ -6,21 +6,20 @@
 setcpm(192/4)
 
 /* instruments
+marimba
 xylophone_soft_ff
 xylophone_medium_ff
-marimba
-gm_marimba
-gm_xylophone
 */
 
+
 // player 1
-$: note("f#3 b3 c#4 b3 c#4 f#3 b3 c#4, - d4 e4 d4 f#4 - d4 e4").sound("xylophone_soft_ff").gain(0.5)
+$: note("f#3 b3 c#4 b3 c#4 f#3 b3 c#4, - d4 e4 d4 f#4 - d4 e4").sound("xylophone_soft_ff").gain(0.5).room(.2)
 
 // player 2
-$: note("b2 e3 f#3 e3 f#3 b2 e3 f#3, - g3 a3 g3 b3 - g3 a3").sound("xylophone_soft_ff").gain(0.5)
+$: note("b2 e3 f#3 e3 f#3 b2 e3 f#3, - g3 a3 g3 b3 - g3 a3").sound("xylophone_soft_ff").gain(0.5).room(.2)
 
 // player 3
-$: note("d2 a3 c#3 a3 c#3 d2 a3 c#3, - d3 e3 d3 f#3 - d3 e3").sound("xylophone_soft_ff").gain(0.5)
+$: note("d2 a3 c#3 a3 c#3 d2 a3 c#3, - d3 e3 d3 f#3 - d3 e3").sound("xylophone_soft_ff").gain(0.5).room(.2)
 
 // player 4
 $: note("-")
@@ -29,4 +28,4 @@ $: note("-")
 $: note("-")
 
 // player 6
-$: note("f#3 b3 c#4 b3 c#4 f#3 b3 c#4, - d4 e4 d4 f#4 - d4 e4").sound("xylophone_soft_ff").gain(0.5)
+$: note("f#3 b3 c#4 b3 c#4 f#3 b3 c#4, - d4 e4 d4 f#4 - d4 e4").sound("xylophone_soft_ff").gain(0.5).room(.2)
