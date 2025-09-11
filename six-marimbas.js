ed7125f10b31307fce6257cdbdaeaa18fe7239e1
Jim Thurston <jimthurston@gmail.com>
2025-09-11 22:51:06 +0100

    Update six-marimbas.js

--- six-marimbas.js
+++ six-marimbas.js
@@ -34,27 +34,16 @@ let players45 = "-"
 //players45 = " - c#3 d2 a2 c#3 a2 c#3 d2, - e3 - d3 e3 d3 f#3 -"
 //players45 = " a2 c#3 d2 a2 c#3 a2 c#3 d2, d3 e3 - d3 e3 d3 f#3 -"
 
+
 // players 1, 2, 3, 6
 $: note(players1236)
   .sound(instrument)
   .gain(gainMain)
   .room(reverb)
-
-// player 2
-//$: note("b2 e3 f#3 e3 f#3 b2 e3 f#3, - g3 a3 g3 b3 - g3 a3").sound(instrument).gain(gainMain).room(reverb).pan(0.3)
-
-// player 3
-//$: note("d2 a3 c#3 a3 c#3 d2 a3 c#3, - d3 e3 d3 f#3 - d3 e3").sound(instrument).gain(gainMain).room(reverb).pan(0.7)
+  .pan(0.5)
 
 // players 4 & 5 (start) - uncomment to bring each part in
 $: note(players45)
   .sound(instrument)
   .gain(gainOthers)
   .room(reverb)
-
-// player 4
-
-// player 5
-
-// player 6 (same as player 1 to start)
-//$: note("f#3 b3 c#4 b3 c#4 f#3 b3 c#4, - d4 e4 d4 f#4 - d4 e4").sound(instrument).gain(gainMain).room(reverb).pan(1)
