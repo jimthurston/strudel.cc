95357b7fba942ca6f25194fd588f2bcd70e0b72d
Jim Thurston <jimthurston@gmail.com>
2025-09-11 17:27:28 +0100

    Update six-marimbas.js

--- six-marimbas.js
+++ six-marimbas.js
@@ -5,36 +5,56 @@
 
 setcpm(192/4)
 
-/* instruments
-marimba
-xylophone_soft_ff
-xylophone_medium_ff
-*/
-
-const instrument = "xylophone_soft_ff"
+const instrument = "xylophone_soft_ff" //xylophone_medium_ff, marimba too quiet!
 const gainMain = 0.5
 const gainOthers = 1.5
 const reverb = 0.2
 
-// player 1
-$: note("f#3 b3 c#4 b3 c#4 f#3 b3 c#4, - d4 e4 d4 f#4 - d4 e4").sound(instrument).gain(gainMain).room(reverb).pan(0.3)
+//all(pianoroll)
+
+let players1236 =
+`
+f#3  b3   c#4  b3   c#4  f#3  b3   c#4,
+-    d4   e4   d4   f#4  -    d4   e4,
+b2   e3   f#3  e3   f#3  b2   e3   f#3,
+-    g3   a3   g3   b3   -    g3   a3,
+d2   a3   c#3  a3   c#3  d2   a3   c#3,
+-    d3   e3   d3   f#3  -    d3   e3,
+f#3  b3   c#4  b3   c#4  f#3  b3   c#4,
+-    d4   e4   d4   f#4  -    d4   e4
+`
+
+let players45 = "-"
+//players45 = "- - - - - - c#3 -, - - - - - - f#3 -"
+//players45 = " - - d2 - - - c#3 -, - - - - - - f#3 -"
+//players45 = " - - d2 - c#3 - c#3 -, - - - - e3 - f#3 -"
+//players45 = " - - d2 a2 c#3 - c#3 -, - - - d3 e3 - f#3 -"
+//players45 = " - c#3 d2 a2 c#3 - c#3 -, - e3 - d3 e3 - f#3 -"
+//players45 = " - c#3 d2 a2 c#3 - c#3 d2, - e3 - d3 e3 - f#3 -"
+//players45 = " - c#3 d2 a2 c#3 a2 c#3 d2, - e3 - d3 e3 d3 f#3 -"
+//players45 = " a2 c#3 d2 a2 c#3 a2 c#3 d2, d3 e3 - d3 e3 d3 f#3 -"
+
+// players 1, 2, 3, 6
+$: note(players1236)
+  .sound(instrument)
+  .gain(gainMain)
+  .room(reverb)
 
 // player 2
-$: note("b2 e3 f#3 e3 f#3 b2 e3 f#3, - g3 a3 g3 b3 - g3 a3").sound(instrument).gain(gainMain).room(reverb).pan(0.4)
+//$: note("b2 e3 f#3 e3 f#3 b2 e3 f#3, - g3 a3 g3 b3 - g3 a3").sound(instrument).gain(gainMain).room(reverb).pan(0.3)
 
 // player 3
-$: note("d2 a3 c#3 a3 c#3 d2 a3 c#3, - d3 e3 d3 f#3 - d3 e3").sound(instrument).gain(gainMain).room(reverb).pan(0.6)
+//$: note("d2 a3 c#3 a3 c#3 d2 a3 c#3, - d3 e3 d3 f#3 - d3 e3").sound(instrument).gain(gainMain).room(reverb).pan(0.7)
 
-// players 4 & 5 (start) - uncomment to bring in
-$: note("- - - - - - c#3 -, - - - - - - f#3 -").sound(instrument).gain(gainOthers).room(reverb)
-$: note("- - d2 - - - - -, - - - - - - - -").sound(instrument).gain(gainOthers).room(reverb)
-$: note("- - - - c#3 - - -, - - - - e3 - - -").sound(instrument).gain(gainOthers).room(reverb)
-$: note("- - - a2 - - - -, - - - d3 - - - -").sound(instrument).gain(gainOthers).room(reverb)
-$: note("- c#3 - - - - - -, - e3 - - - - - -").sound(instrument).gain(gainOthers).room(reverb)
+// players 4 & 5 (start) - uncomment to bring each part in
+$: note(players45)
+  .sound(instrument)
+  .gain(gainOthers)
+  .room(reverb)
 
 // player 4
 
 // player 5
 
 // player 6 (same as player 1 to start)
-$: note("f#3 b3 c#4 b3 c#4 f#3 b3 c#4, - d4 e4 d4 f#4 - d4 e4").sound(instrument).gain(gainMain).room(reverb).pan(0.7)
+//$: note("f#3 b3 c#4 b3 c#4 f#3 b3 c#4, - d4 e4 d4 f#4 - d4 e4").sound(instrument).gain(gainMain).room(reverb).pan(1)
