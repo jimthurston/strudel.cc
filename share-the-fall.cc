d09b1071dc2197fdc53cb0e9ed6c82757c1bb998
Jim Thurston <jimthurston@gmail.com>
2025-11-17 16:31:30 +0000

    Add initial share-the-fall.cc script

---
+++ share-the-fall.cc
@@ -0,0 +1,11 @@
+// Share The Fall by Roni Size
+// script @by jimthurston
+
+setcpm(140/4)
+
+// bass
+$: note("[44 - - - - 51 - - 56 - - 51 - - 49 -]/2").sound("gm_bassoon")
+
+// drums
+$: s("hh!4").bank("alesissr16").gain("0.5 0.7 0.5 0.3")
+//$: s("bd sd bd sd").bank("alesissr16")
