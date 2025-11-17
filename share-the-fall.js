9d28ce717f55130d3f43637e4c3d37247632b1fd
Jim Thurston <jimthurston@gmail.com>
2025-11-17 16:57:18 +0000

    Update drum pattern in share-the-fall.js

--- share-the-fall.js
+++ share-the-fall.js
@@ -11,4 +11,4 @@ $: note("[<43 -> - - - - 50 - - 55 - - 50 - - 48 <43 ->]/2").sound("gm_acoustic_
 
 // drums
 $: s("hh!4").bank("alesissr16").gain("0.5 0.7 0.5 0.3")
-//$: s("bd sd bd sd").bank("alesissr16")
+$: s("[bd - sd - - bd - sd - bd sd - bd sd - bd]/2").bank("alesissr16")
